00683cc9d77175ecac5d4cebb913044914c2033b
tuejoshua <65186094+tuejoshua@users.noreply.github.com>
2025-05-14 15:29:07 +0300

    (temp index_,html) Projects: Use Markdown files instead of JSON fields

---
+++ Hilchot Brachot games.md
@@ -0,0 +1,13 @@
+**Summary:** A suite of edutainment games aimed at exercising one's knowledge of Hilchot Brachot (Jewish law wrt. blessings).
+NB: I take no responsibility for 
+For more information, see [the game suite index page].
+
+**Motivation:** When I was learning (the "Order of Priority/Precedence") part of these laws, I was having difficulty obtaining a logical understanding of them. Having to resort to memorization seemed unpleasing, and I realized that I would have to "gamify" this knowledge somehow if I wanted to retain it.
+I quickly realized that such games might be interesting to others of all ages and nationalities, easily be made interesting for and accessible to a broader audience, interesting to other adults, (mine as well as other) children
+...and other games have since been added to the suite.
+
+**Goals:**
+- Ease of use for all ages and nationalities
+- As lightweight as possible, while using multimedia 
+- Front-end only
+**Learnings:** This project taught me a lot about multi-page website architecture and navigation, [LocalStorage],
---
+++ displayProjectCards.js
@@ -0,0 +1,118 @@
+// Import Markdown parser as ES module
+//... for this to work, this (.js) file must itself be included (by calling .html file as such a module (i.e. "<script type=module>")
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
+
+function getTechLogo(tag) {
+	switch(tag) {
+		case 'FrontEnd':
+			logoUrl = 'front-end-logo-color (from github.com-shannonmoeller).png';
+			//licenseLink = 'github.com/shannonmoeller/front-end-logo';
+			altText = 'Front End';
+			break;
+		case 'JSON':
+			logoUrl = 'JSON_vector_logo.svg (from Wikimedia Commons).png';
+			//licenseLink = 'commons.wikimedia.org/wiki/File:JSON_vector_logo.svg';
+			altText = 'JSON';
+
+			break;
+		case 'PHP':
+			logoUrl = 'new-php-logo (from php.net).png';
+			//licenseLink = 'www.php.net/download-logos.php';
+			altText = 'PHP';
+			break;
+		case 'Python':
+			logoUrl = 'python-logo-only (from python.org).png';
+			//licenseLink = 'www.python.org/community/logos/';
+			altText = 'Python';
+			break;
+		default:
+			alert('logo not found for "' + tag + '"');
+			return '';
+	}
+	return '<img alt="' + altText + ' icon" src="resources/' + logoUrl + '" height=40 title="' + altText + '.\nFor source and license of this icon, see further down this page">';
+}
+
+function showProjectCard(i) {
+	switch (document.getElementById('projectCard' + i).style.display) {
+		case 'none':
+			document.getElementById('projectCard' + i).style.display = 'block';
+			break;
+		case 'block':
+		document.getElementById('projectCard' + i).style.display = 'none';
+	}
+}
+
+/* The following is based on an example on https://www.w3schools.com/jsref/api_fetch.asp
+...yes, previously using a "new XMLHttpRequest()"
+...and of course, one COULD just use React, by it seems I'm still a bit of a DIY fanatic
+*/
+
+async function displayProjectCards(file) {
+
+	let projectsJsonObject = await fetch(file);
+	let projects = await projectsJsonObject.json();
+  
+	var domString = '';
+     
+	for (i=0; i<projects.length; i++) {
+		
+		fontSizeParam = 'font-size: 1.3em';
+		
+		// adding "max-height: 999999px;" style to button element, as per suggestion from chatGPT to fix problem I'm seeing specifically in Chrome for Android
+		// also, without forcing "text-align: left" here (as should be the default), many browsers use "justify" instead (which I dislike)
+		domString += '<button type="button" style="width: 100%; text-align: left; style="max-height: 999999px;" onclick="showProjectCard(' + i + ');">' +
+			'<table style="width: 100%; border-collapse: collapse;"><tr><td style="border: none; text-align: left; ' + fontSizeParam + '">' + projects[i].title + '</td><td style="text-align: right; border: none;">';
+			
+		for (let tag of projects[i].technologies) {
+				domString += getTechLogo(tag);
+		}
+		
+	/// PROJECT DESCRIPTION FROM MARKDOWN FILE
+
+		// Read assumed Markdown file; alert if problem
+		// projects[i].title + '.md'
+		// Error handling based on chatGPT suggestion
+		try	{
+			let mdFileObject = await fetch(projects[i].title + '.md');
+			if (!mdFileObject.ok) { // fetch does not throw catchable error itself
+				throw new Error(`HTTP error! status: ${mdFileObject.status}`);
+			}
+			let mdContent = await mdFileObject.text();
+			domString += marked.parse(mdContent);
+		}
+		catch (error) {
+		}
+				domString += '<p style="color:red;"><em>Could not load project description from file ' + projects[i].title + '.md:<br>' + error + '</em></p>';
+				
+		/*
+		motivationString = projects[i].motivation.trim();
+		if (motivationString !== '') {
+			motivationString = '<b>motivation:</b><br>' + motivationString;
+		}
+		
+		summaryString = projects[i].summary.trim();
+		if (summaryString !== '') {
+			summaryString = '<b>summary: </b><br>' + summaryString;
+		}
+
+		detailsString = projects[i].details.trim();
+		if (detailsString !== '') {
+			detailsString = '<b>details: </b><br>' + detailsString;
+		}
+		
+		*/
+		
+		// max-width because width does not include the padding that were also applying
+		domString += '</td></tr></table></button>' +
+			'<div id="projectCard' + i + '" style="max-width: 100%; display: none; border-left-style: dotted; padding: 15px;">' +
+			marked.parse(FILE CONTENT) +
+			'</div>';
+			
+			//motivationString + '<p>' + summaryString + '<p>' + detailsString 
+				
+    }
+            
+    // Write HTML DOM string to DIV element
+    document.getElementById('showData').innerHTML = domString;
+		
+}
