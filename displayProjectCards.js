04485daa0eb57db171009775d88f1307b0ba29ad
tuejoshua <65186094+tuejoshua@users.noreply.github.com>
2025-07-27 21:19:57 +0300

    Add ▶/▼ to clickable buttons

--- displayProjectCards.js
+++ displayProjectCards.js
@@ -32,14 +32,11 @@ function getTechLogo(tag) {
 	return '<img alt="' + altText + ' icon" src="resources/' + logoUrl + '" height=40 title="' + altText + '.\nFor source and license of this icon, see further down this page">';
 }
 
-function showProjectCard(i) {
-	switch (document.getElementById('projectCard' + i).style.display) {
-		case 'none':
-			document.getElementById('projectCard' + i).style.display = 'block';
-			break;
-		case 'block':
-		document.getElementById('projectCard' + i).style.display = 'none';
-	}
+function showProjectCard(btn) {
+	const card = document.getElementById('projectCard' + btn.dataset.index); // DATA-INDEX parameter);
+	const isVisible = card.style.display === 'block';
+	card.style.display = isVisible? 'none' : 'block';
+	btn.textContent = (isVisible ? '▶' : '▼') + ' ' + btn.textContent.slice(2);
 }
 
 /* The following is based on an example on https://www.w3schools.com/jsref/api_fetch.asp
@@ -63,7 +60,7 @@ export async function displayProjectCards(file) {
 		// ...
 		// ONCLICK is obsolete - and breaks with MODULE code => CLASS + DATA-* + event listener elsewhere...
 		domString += '<button type="button" class="ProjectButton" data-index="' + i + '" style="width: 100%; text-align: left; style="max-height: 999999px;">' +
-			'<table style="width: 100%; border-collapse: collapse;"><tr><td style="border: none; text-align: left; ' + fontSizeParam + '">' + projects[i].title + '</td><td style="text-align: right; border: none;">';
+			'<table style="width: 100%; border-collapse: collapse;"><tr><td style="border: none; text-align: left; ' + fontSizeParam + '">' + '▶ ' + projects[i].title + '</td><td style="text-align: right; border: none;">';
 			
 		for (let tag of projects[i].technologies) {
 				domString += getTechLogo(tag);
@@ -113,8 +110,7 @@ export async function displayProjectCards(file) {
 	// OnClick events (see ONCLICK comment above)
 	document.querySelectorAll('.ProjectButton').forEach(btn => { // all CLASS = ProjectButton
 		btn.addEventListener('click', () => {
-			const i = btn.dataset.index; // DATA-INDEX parameter
-			showProjectCard(i);
+			showProjectCard(btn);
 		});
 	});
 
