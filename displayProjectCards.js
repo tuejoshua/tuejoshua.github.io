18b4d8fb4997157674d4911449a2cc708176fa4a
tuejoshua <65186094+tuejoshua@users.noreply.github.com>
2025-09-08 20:01:44 +0300

    - Move general style from specific to base .css - Move (some) specific style from .js to specific .css - Update comments

--- displayProjectCards.js
+++ displayProjectCards.js
@@ -52,11 +52,9 @@ export async function displayProjectCards(file) {
 
 	for (let i=0; i<projects.length; i++) {
 
-		const fontSizeParam = 'font-size: 1.3em';
-
 		// ONCLICK is obsolete - and breaks with MODULE code => CLASS + DATA-* + event listener elsewhere...
 		domString += '<button type="button" class="ProjectButton" data-index="' + i + '">' +
-			'<table style="width: 100%; border-collapse: collapse;"><tr><td class="project-title-cell" style="border: none; text-align: left; ' + fontSizeParam + '">' + '▶ ' + projects[i].title + '</td><td style="text-align: right; border: none;">';
+			'<table style="width: 100%; border-collapse: collapse;"><tr><td class="project-title-cell" style="border: none; text-align: left; font-size: 1.3em;">' + '▶ ' + projects[i].title + '</td><td style="text-align: right; border: none;">';
 
 		for (let tag of projects[i].technologies) {
 				domString += getTechLogo(tag);
@@ -66,7 +64,7 @@ export async function displayProjectCards(file) {
 
 		// max-width because width does not include the padding that were also applying
 		domString += '</td></tr></table></button>' +
-			'<div id="projectCard' + i + '" class="markdown-content" style="max-width: 100%; display: none; border-left-style: solid; border-color: lightgray; padding: 15px;">';
+			'<div id="projectCard' + i + '" class="markdown-content">';
 
 		// Read assumed Markdown file; alert if problem
 		// projects[i].title + '.md'
